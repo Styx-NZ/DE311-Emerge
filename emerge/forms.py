5cf976d059c98cbe3fa59d41154568a7ea19e171
tid0093 <140573995+tid0093@users.noreply.github.com>
2025-07-23 21:36:05 +1200

    files from onedrive

--- emerge/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Project
-
-class ProjectCreate(forms.ModelForm):
-    class Meta:
-        model = Project
-        fields = ['student_name', 'year', 'semester', 'student_photo', 'project_photo', 'short_paper', 'project_name']
